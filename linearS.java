418cebbec5f20428768cdc4ffd75c82b0ea2b9c0
aakashkaler <68342702+aakashkaler@users.noreply.github.com>
2020-07-19 22:29:43 +0530

    Create linearS.java

---
+++ linearS.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class linearS {
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(),k,i;
+        System.out.println("enter element to be search");
+        k=scan.nextInt();
+        int a[]=new int[n];
+        
+        for (i = 0; i < n; i++)
+         {
+            a[i]=scan.nextInt();
+        }
+        scan.close();
+        for (i = 0; i < n-1; i++)
+         {
+           if(k==a[i])
+           {
+               break;
+           }
+        }
+        System.out.println("element found at "+i);
+    
+
+    }
+}
